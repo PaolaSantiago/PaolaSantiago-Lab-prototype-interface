2af788024d48ee672a9262093f8138a7b8f034aa
PaolaSantiago <47189459+PaolaSantiago@users.noreply.github.com>
2019-02-20 14:52:59 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-
+let appoinment1Structure = [
+    {
+      name: 'Frank'
+      location: 'downtown'
+      day: 'February 1st'
+      weekday: 'Friday'
+      time: '9:00 am'
+    },
+    {
+        name: 'Kylie'
+        location: 'ajax'
+        day: 'February 2nd'
+        weekday: 'Saturday'
+        time: '10:00 am'
+      },
+      {
+          name: 'Markus'
+          location: 'downtown'
+          day: 'February 4'
+          weekday: 'Monday'
+          time: '11:00 am'
+        },
+        {
+            name: 'Ronald'
+            location: 'ajax'
+            day: 'February 8'
+            weekday: 'Friday'
+            time: '13:00 pm'
+          },
+          {
+              name: 'Ronald'
+              location: 'ajax'
+              day: 'February 8'
+              weekday: 'Friday'
+              time: '13:00 pm'
+            },
