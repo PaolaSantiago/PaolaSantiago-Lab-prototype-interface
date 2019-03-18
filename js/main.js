38e92e3c473235bf34ede92fa553e27d41ee4007
PaolaSantiago <47189459+PaolaSantiago@users.noreply.github.com>
2019-03-18 15:31:29 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,65 @@
-let appoinment1Structure = [
+
+
+  let appoinment1Structure = ['frank','Kylie','Markus','Ronald','Emily'];
+  console.log(appoinment1Structure);
+
+  let appointments = [
     {
-      name: 'Frank'
-      location: 'downtown'
-      day: 'February 1st'
-      weekday: 'Friday'
-      time: '9:00 am'
+      name: 'Frank',
+      location: 'downtown',
+      date: {
+        year: 2019,
+        month: 2,
+        day: 1,
+        time: '9:00',
+      }
     },
+
+
     {
-        name: 'Kylie'
-        location: 'ajax'
-        day: 'February 2nd'
-        weekday: 'Saturday'
-        time: '10:00 am'
-      },
+      name: 'Kylie',
+      location: 'ajax',
+      date: {
+        year: 2019
+        month: 2,
+        day: 2,
+        time: '10:00',
+      }
+    },
+
       {
-          name: 'Markus'
-          location: 'downtown'
-          day: 'February 4'
-          weekday: 'Monday'
-          time: '11:00 am'
-        },
+
+        name: 'Markus',
+        location: 'downtown',
+        date: {
+          year: 2019
+          month: 2,
+          day: 6,
+          time: '11:00',
+        }
+      },
+
+      {
+
+        name: 'Ronald',
+        location: 'ajax',
+        date: {
+          year: 2019
+          month: 2,
+          day: 8,
+          time: '13:00',
+         }
+       },
+
         {
-            name: 'Ronald'
-            location: 'ajax'
-            day: 'February 8'
-            weekday: 'Friday'
-            time: '13:00 pm'
-          },
-          {
-              name: 'Ronald'
-              location: 'ajax'
-              day: 'February 8'
-              weekday: 'Friday'
-              time: '13:00 pm'
-            },
+
+            name: 'Emily',
+            location: 'downtown',
+            date: {
+              year: 2019
+              month: 2,
+              day: 11,
+              time: '15:00',
+            }
+          {,
+      ];
