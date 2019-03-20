81e01868b277765698b50658b427505d5c0f80cf
PaolaSantiago <47189459+PaolaSantiago@users.noreply.github.com>
2019-03-20 15:18:38 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@
       name: 'Kylie',
       location: 'ajax',
       date: {
-        year: 2019
+        year: 2019,
         month: 2,
         day: 2,
         time: '10:00',
@@ -32,7 +32,7 @@
         name: 'Markus',
         location: 'downtown',
         date: {
-          year: 2019
+          year: 2019,
           month: 2,
           day: 6,
           time: '11:00',
@@ -44,7 +44,7 @@
         name: 'Ronald',
         location: 'ajax',
         date: {
-          year: 2019
+          year: 2019,
           month: 2,
           day: 8,
           time: '13:00',
@@ -56,10 +56,18 @@
             name: 'Emily',
             location: 'downtown',
             date: {
-              year: 2019
+              year: 2019,
               month: 2,
               day: 11,
               time: '15:00',
             }
-          {,
+          }
       ];
+
+
+// Loop that runs 31 times
+for (var d = 1; d <= 31; d++) {
+  var apptHTML = appointments.filter(apt => apt.date.day == d).map(apt => `<div class="apt">${apt.name}</div>`).join('');
+  if (apptHTML != '')
+    document.querySelector(`[data-day="${d}"]`).innerHTML += apptHTML;
+}
